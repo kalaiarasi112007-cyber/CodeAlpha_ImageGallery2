6a681509850d65da2b3ca01e3ffe3c869a4a09b5
kalaiarasi112007-cyber <kalaiarasi112007@gmail.com>
2026-04-01 21:43:22 +0530

    Add files via upload
    My Image Gallery Project

---
+++ script.js
@@ -0,0 +1,16 @@
+function openImage(img) {
+    document.getElementById("lightbox").style.display = "block";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeImage() {
+    document.getElementById("lightbox").style.display = "none";
+}
+function openImage(img) {
+    document.getElementById("lightbox").style.display = "block";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeImage() {
+    document.getElementById("lightbox").style.display = "none";
+}
